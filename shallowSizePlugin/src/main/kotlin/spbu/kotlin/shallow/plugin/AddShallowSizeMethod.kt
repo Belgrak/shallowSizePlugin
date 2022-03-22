6c780e748b83d5fd2f21710c927bb46e2aa17d0c
Glebus <glekar03@mail.ru>
2022-03-22 20:44:39 +0300

    replace all TODOs

--- shallowSizePlugin/src/main/kotlin/spbu/kotlin/shallow/plugin/AddShallowSizeMethod.kt
+++ shallowSizePlugin/src/main/kotlin/spbu/kotlin/shallow/plugin/AddShallowSizeMethod.kt
@@ -3,22 +3,57 @@ package spbu.kotlin.shallow.plugin
 import arrow.meta.CliPlugin
 import arrow.meta.Meta
 import arrow.meta.invoke
+import arrow.meta.quotes.Transform
 import arrow.meta.quotes.classDeclaration
-import org.jetbrains.kotlin.ir.types.IrType
+import org.jetbrains.kotlin.backend.common.lower.DeclarationIrBuilder
+import org.jetbrains.kotlin.ir.builders.irBlockBody
+import org.jetbrains.kotlin.ir.builders.irInt
+import org.jetbrains.kotlin.ir.builders.irReturn
+import org.jetbrains.kotlin.ir.types.*
+import org.jetbrains.kotlin.ir.util.functions
 
 const val DEFAULT_SIZE = 8
 
-fun IrType.byteSize(): Int =
-    TODO("Calculate types for Char, Byte, Short, Int, Long, UByte, UShort, ULong, Float, Double, Boolean, Unit. For nullable and all other types use DEFAULT_SIZE value")
+fun IrType.byteSize(): Int {
+    return when {
+        isChar() -> 2
+        isByte() || isUByte() -> 1
+        isShort() || isUShort() -> 2
+        isInt() -> 4
+        isLong() || isULong() -> 8
+        isFloat() -> 4
+        isDouble() -> 8
+        isBoolean() -> 1
+        isUnit() -> 4
+        else -> DEFAULT_SIZE
+    }
+}
 
 val Meta.GenerateShallowSize: CliPlugin
     get() = "Generate shallowSize method" {
         meta(
-            classDeclaration(this, { TODO("Check if the current declaration is a data class") }) { declaration ->
-                TODO("Add new shallowSize function without implementation. Please, use arrow.meta.quotes.Transform.replace")
+            classDeclaration(this, { element.isData() }) { declaration ->
+                Transform.replace(
+                    replacing = declaration.element,
+                    newDeclaration = """
+                            |$`@annotations` $kind $name $`(typeParameters)` $`(params)` : $superTypes"{
+                            |   $body
+                            |   fun void shallowSize(): Int 
+                            | } """.`class`.syntheticScope
+                )
             },
             irClass { clazz ->
-                TODO("Only for data classes calculate the sum of sized its properties and replace the shallowSize function body, please use org.jetbrains.kotlin.backend.common.lower.DeclarationIrBuilder, org.jetbrains.kotlin.ir.builders.irBlockBody, and org.jetbrains.kotlin.ir.builders.irInt to build new function body")
+                if (clazz.isData) {
+                    var sumOfSized = 0
+                    for (element in clazz.superTypes) {
+                        sumOfSized += element.byteSize()
+                    }
+                    clazz.functions.find { it.name.toString() == "shallowSize" }?.let { shallowSize ->
+                        shallowSize.body = DeclarationIrBuilder(pluginContext, shallowSize.symbol).irBlockBody {
+                            +irReturn(irInt(sumOfSized))
+                        }
+                    }
+                }
                 clazz
             }
         )
