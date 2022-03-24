8649cba76c17a7993ca17690eada1b1ee10d00a4
Glebus <glekar03@mail.ru>
2022-03-24 19:20:55 +0300

    fix: add detekt in build.gradle.kts and format AddShallowSizeMethod.kt

--- shallowSizePlugin/src/main/kotlin/spbu/kotlin/shallow/plugin/AddShallowSizeMethod.kt
+++ shallowSizePlugin/src/main/kotlin/spbu/kotlin/shallow/plugin/AddShallowSizeMethod.kt
@@ -9,23 +9,37 @@ import org.jetbrains.kotlin.backend.common.lower.DeclarationIrBuilder
 import org.jetbrains.kotlin.ir.builders.irBlockBody
 import org.jetbrains.kotlin.ir.builders.irInt
 import org.jetbrains.kotlin.ir.builders.irReturn
-import org.jetbrains.kotlin.ir.types.*
+import org.jetbrains.kotlin.ir.types.IrType
+import org.jetbrains.kotlin.ir.types.isByte
+import org.jetbrains.kotlin.ir.types.isChar
+import org.jetbrains.kotlin.ir.types.isUByte
+import org.jetbrains.kotlin.ir.types.isShort
+import org.jetbrains.kotlin.ir.types.isUShort
+import org.jetbrains.kotlin.ir.types.isInt
+import org.jetbrains.kotlin.ir.types.isLong
+import org.jetbrains.kotlin.ir.types.isULong
+import org.jetbrains.kotlin.ir.types.isFloat
+import org.jetbrains.kotlin.ir.types.isDouble
+import org.jetbrains.kotlin.ir.types.isBoolean
+import org.jetbrains.kotlin.ir.types.isUnit
 import org.jetbrains.kotlin.ir.util.functions
 import org.jetbrains.kotlin.ir.util.properties
 
 const val DEFAULT_SIZE = 8
+const val BOOLEAN_SIZE = 1
+const val UNIT_SIZE = 8
 
 fun IrType.byteSize(): Int {
     return when {
-        isChar() -> 2
-        isByte() || isUByte() -> 1
-        isShort() || isUShort() -> 2
-        isInt() -> 4
-        isLong() || isULong() -> 8
-        isFloat() -> 4
-        isDouble() -> 8
-        isBoolean() -> 1
-        isUnit() -> 4
+        isChar() -> Char.SIZE_BYTES
+        isByte() || isUByte() -> Byte.SIZE_BYTES
+        isShort() || isUShort() -> Short.SIZE_BYTES
+        isInt() -> Int.SIZE_BYTES
+        isLong() || isULong() -> Long.SIZE_BYTES
+        isFloat() -> Float.SIZE_BYTES
+        isDouble() -> Double.SIZE_BYTES
+        isBoolean() -> BOOLEAN_SIZE
+        isUnit() -> UNIT_SIZE
         else -> DEFAULT_SIZE
     }
 }
